971fb3ad99d8c2ec6d81ed982aa369abce048c2a
Muhammed Thansil <mdtlofficial@gmail.com>
2025-01-31 07:53:52 +0530

    init

---
+++ inertia/pages/home.tsx
@@ -0,0 +1,9 @@
+import { Head } from '@inertiajs/react'
+export default function Home() {
+  return (
+    <div>
+      <Head title="Welcome Home" />
+      <h1>Welcome Home</h1>
+    </div>
+  )
+}
